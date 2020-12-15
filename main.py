60ffab25c25dff6012263078c440d868f177d3c8
Artem Zaitsev <artz1994@mail.ru>
2020-12-15 18:54:57 +0300

    cors

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from routes import routes
 
 app = FastAPI()
 app.include_router(routes)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
