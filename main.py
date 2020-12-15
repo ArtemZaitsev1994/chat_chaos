5581cd6e2e080d9c4ed587b8204901faddc1c0bd
Artem Zaitsev <artz1994@mail.ru>
2020-12-15 21:23:54 +0300

    a

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ app = FastAPI()
 app.include_router(routes)
 
 origins = [
-    "*",
+    "http://localhost:3001",
+    "https://localhost:3001",
+    "http://192.168.101.75:3001",
+    "http://134.122.73.229:3050"
+]
+
+headers = [
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Credentials',
+    'set-cookie'
 ]
 
 app.add_middleware(
@@ -14,5 +23,5 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=headers,
 )
